85fe90a95752fbd734b40a71f9c2f355e571f44f
Minh13112003 <nguyennhatminhhbt@gmail.com>
2024-10-15 16:48:12 +0700

    first commit

---
+++ PBL03/DAL/OrderFood_DAL.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBL03.DAL
+{
+    internal class OrderFood_DAL
+    {
+        public void getFood_DAL(FlowLayoutPanel flowLayoutPanel)
+        {
+            using (var db = new PBL3Entities1())
+            {
+                var data = db.Foods.Where(p => p.IDCategory == "FD").Select(p => new
+                {
+                    p.NameFood,
+                    p.Price,
+                    p.PictureFood
+                }).ToList();
+                foreach (var item in data)
+                {
+                    // Tạo một đối tượng UserControl_Menu mới
+                    UserControl_Food uf = new UserControl_Food();
+
+                    // Truyền giá trị cho các label của user control
+                    
+                    uf.lbFood.Text = item.NameFood.ToString();
+                    uf.lbPrice.Text = item.Price.ToString() + " VND";
+                    string imagepath = Path.Combine(Application.StartupPath, item.PictureFood.ToString()); 
+                    //Image img = Image.FromFile(imagepath);
+                    //uf.BackgroundImage = img;
+
+                    byte[] imagedata = File.ReadAllBytes(imagepath);
+                    using (MemoryStream ms = new MemoryStream(imagedata))
+                    {
+                        Image img = Image.FromStream(ms);
+                        uf.BackgroundImage = img;
+                    }
+
+
+                    // Thêm user control vào FlowLayoutPanel
+                    flowLayoutPanel.Controls.Add(uf);
+                }
+            }
+        }
+        public void getDrink_DAL(FlowLayoutPanel flowLayoutPanel)
+        {
+            using (var db = new PBL3Entities1())
+            {
+                var data = db.Foods.Where(p => p.IDCategory == "DR").Select(p => new
+                {
+                    p.NameFood,
+                    p.Price,
+                    p.PictureFood
+                }).ToList();
+                foreach (var item in data)
+                {
+                    // Tạo một đối tượng UserControl_Menu mới
+                    UserControl_Food uf = new UserControl_Food();
+
+                    // Truyền giá trị cho các label của user control
+
+                    uf.lbFood.Text = item.NameFood.ToString();
+                    uf.lbPrice.Text = item.Price.ToString() + " VND";
+                    string imagepath = Path.Combine(Application.StartupPath, item.PictureFood.ToString());
+                    //Image img = Image.FromFile(imagepath);
+                    //uf.BackgroundImage = img;
+
+                    byte[] imagedata = File.ReadAllBytes(imagepath);
+                    using (MemoryStream ms = new MemoryStream(imagedata))
+                    {
+                        Image img = Image.FromStream(ms);
+                        uf.BackgroundImage = img;
+                    }
+
+
+                    // Thêm user control vào FlowLayoutPanel
+                    flowLayoutPanel.Controls.Add(uf);
+                }
+            }
+        }
+        public void getCreams_DAL(FlowLayoutPanel flowLayoutPanel)
+        {
+            using (var db = new PBL3Entities1())
+            {
+                var data = db.Foods.Where(p => p.IDCategory == "CR").Select(p => new
+                {
+                    p.NameFood,
+                    p.Price,
+                    p.PictureFood
+                }).ToList();
+                foreach (var item in data)
+                {
+                    // Tạo một đối tượng UserControl_Menu mới
+                    UserControl_Food uf = new UserControl_Food();
+
+                    // Truyền giá trị cho các label của user control
+
+                    uf.lbFood.Text = item.NameFood.ToString();
+                    uf.lbPrice.Text = item.Price.ToString() + " VND";
+                    string imagepath = Path.Combine(Application.StartupPath, item.PictureFood.ToString());
+                    //Image img = Image.FromFile(imagepath);
+                    //uf.BackgroundImage = img;
+
+                    byte[] imagedata = File.ReadAllBytes(imagepath);
+                    using (MemoryStream ms = new MemoryStream(imagedata))
+                    {
+                        Image img = Image.FromStream(ms);
+                        uf.BackgroundImage = img;
+                    }
+
+
+                    // Thêm user control vào FlowLayoutPanel
+                    flowLayoutPanel.Controls.Add(uf);
+                }
+            }
+        }
+        public void orderMeal_DAL(FlowLayoutPanel flowLayoutPanel, string tb)
+        {
+            using (var db = new PBL3Entities1())
+            {
+                var query = from p in db.OrderTables
+                            join c in db.Foods on p.IDFood equals c.ID_Food
+                            where p.IDTable == tb
+                            select new
+                            {
+                                p.ID_Order,
+                                c.NameFood,
+                                p.Quantity,
+                                p.IDTable,
+                                c.Price
+                            };
+                var order = db.OrderTables
+                    .Join(db.Foods, p => p.IDFood, c => c.ID_Food, (p, c) => new { OrderTable = p, Food = c })
+                    .Where(x => x.OrderTable.IDTable == tb)
+                    .Select(x => new
+                    {
+                        x.OrderTable.ID_Order,
+                        x.Food.NameFood,
+                        x.OrderTable.Quantity,
+                        x.OrderTable.IDTable,
+                        x.Food.Price
+                    }).ToList();
+                foreach (var item in order)
+                {
+                    UserControl_Order uo = new UserControl_Order();
+                    uo.lbFood.Text = item.NameFood.ToString();
+                    uo.lbNameTable.Text = item.IDTable.ToString();
+                    uo.lbPrice.Text = item.Price.ToString() + " VND";
+                    uo.numericquantity.Text = item.Quantity.ToString();
+                    flowLayoutPanel.Controls.Add(uo);
+                }
+            }
+        }
+    }
+}
---
+++ PBL03/UserControl_Food.cs
@@ -0,0 +1,58 @@
+﻿using PBL03.BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBL03
+{
+    public partial class UserControl_Food : UserControl
+    {
+        private Form_Order f;
+        private OrderFood_BLL bll;
+        public UserControl_Food()
+        {
+            InitializeComponent();
+            bll = new OrderFood_BLL();
+            f = new Form_Order();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            if (btnSelect.Visible == true)
+            {
+                btnSelect.Visible = false;
+            }
+            else
+            {
+                btnSelect.Visible = true;
+            }
+        }
+
+        private void UserControl_Click(object sender, EventArgs e)
+        {
+            if (btnSelect.Visible == true)
+            {
+                btnSelect.Visible = false;
+            }
+            else
+            {
+                btnSelect.Visible = true;
+            }
+            f.flowLayout_Order.Controls.Clear();
+            OrderFood_BLL bll = new OrderFood_BLL();
+            bll.orderMeal_BLL(f.flowLayout_Order, "B1");
+            //UserControl_Order uo = new UserControl_Order();
+            //uo.lbFood.Text = "Cá viên chiên";
+            //uo.lbNameTable.Text = "B1";
+            //uo.lbPrice.Text = "15000 VND";
+            //uo.numericquantity.Value = 1;
+            //f.flowLayout_Order.Controls.Add(uo);
+        }
+    }
+}
